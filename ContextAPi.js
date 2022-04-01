0640a39b3f043dad22d63b00c4502274bc28e4f8
niiyazobug <niiyaz5555@gmail.com>
2022-04-01 17:58:47 +0600

    .

--- ContextAPi.js
+++ ContextAPi.js
@@ -3,7 +3,7 @@
  * context api
  * 1. call createContext with a default value.
  * 2. set a variable of the context with uppercase.
- * 3. Maake sure you export the context to use it in other place.
+ * 3. Make sure you export the context to use it in other place.
  * 4.Wrap you child content using RingContext.Provider.
  * 5. Provide a value.
  * 6. to consume the context from child component.
